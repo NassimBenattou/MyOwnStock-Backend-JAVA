5d51c95f6df349a83a6600cb160c8375999ca2d5
Nassim <nassim.benattou13@gmail.com>
2024-01-11 13:18:15 +0100

    add cors config

---
+++ product/src/main/java/org/aelion/product/CorsConfig.java
@@ -0,0 +1,19 @@
+package org.aelion.product;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8081")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("Origin", "Content-Type", "Accept", "Authorization")
+                .allowCredentials(true);
+    }
+}
--- product/src/main/java/org/aelion/product/ProductApplication.java
+++ product/src/main/java/org/aelion/product/ProductApplication.java
@@ -1,13 +1,22 @@
 package org.aelion.product;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProductApplication {
+public class ProductApplication implements WebMvcConfigurer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductApplication.class, args);
 	}
 
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+				.allowedOrigins("http://localhost:8081")
+				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+				.allowedHeaders("Origin", "Content-Type", "Accept", "Authorization")
+				.allowCredentials(true);
+	}
 }
